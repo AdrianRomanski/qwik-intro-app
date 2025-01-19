911bd4e6bd18635e50eaf889f71fa362b3b3d5c4
adrianromanski <adrian.romanski@codeweaver.pl>
2025-01-19 16:39:34 +0100

    add page 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
-
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
